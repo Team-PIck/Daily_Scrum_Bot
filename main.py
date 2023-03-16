e84edd9d04cee63536019437f959c50a032c4f75
KimBeomJin <sdpthf@naver.com>
2023-03-16 17:19:26 +0900

    슬랙 메시지 전송

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+client = WebClient(token="xoxb-4693014614277-4927023407137-clLiRr2CKEXxVjmx612pdTsW")
+
+def send_message():
+    try:
+        message = "<!channel> 데일리 스크럼 작성해주세요! :rocket:"
+
+        response = client.chat_postMessage(channel="#test", text=message)
+        print(f"메시지 전송 성공: {response['ts']}")
+    except SlackApiError as e:
+        print(f"메시지 전송 실패: {e.response['error']}")
+
+if __name__ == '__main__':
+    send_message()
